c6c064404b6eb1bd5616bdcb14d30d3f647b82fb
Rodrigo Montero Luna - Ceiba Software <rodrigo.montero@ceiba.com.co>
2022-06-06 23:38:10 -0500

    Primer commit

---
+++ src/main/java/com/adn/RetoAdnApplication.java
@@ -0,0 +1,13 @@
+package com.adn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoAdnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adn/RetoAdnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoAdnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
